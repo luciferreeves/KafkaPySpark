223c2151bd7b08c99aa5910a31e0d7d642f2edf4
jmreddy2106 <jmreddy@lbrce.ac.in>
2021-12-23 23:32:03 -0500

    Tested consumer

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # KafkaPySpark
+# Zookeeper
+bin/zookeeper-server-start.sh config/zookeeper.properties
+# Kafka Server
+bin/kafka-server-start.sh config/server.properties
+# Kafka Topic creation
+bin/kafka-topics.sh --create --partitions 2 —replication-factor 1 --topic twitterdata --bootstrap-server localhost:9092
--- consumer.py
+++ consumer.py
@@ -8,39 +8,53 @@ from pyspark.streaming import StreamingContext
 import math
 import string
 import random
+from pyspark.conf import SparkConf
+
 
 topic_name = 'twitterdata'
 
+
+# consumer = KafkaConsumer(topic_name)
+# for msg in consumer:
+#     print (msg)
+
 consumer = KafkaConsumer(
     topic_name,
-    bootstrap_servers=['localhost:9092'],
     auto_offset_reset='latest',
     enable_auto_commit=True,
     auto_commit_interval_ms =  5000,
     fetch_max_bytes = 128,
     max_poll_records = 100,
-
     value_deserializer=lambda x: json.loads(x.decode('utf-8')))
 
-if __name__ == '__main__':
-    spark = SparkSession.builder.appName("TwitterStreaming").getOrCreate().readStream.format("kafka").option("kafka.bootstrap.servers", "localhost:9092").option("subscribe", topic_name).load()
-    spark.createOrReplaceTempView("tweets")
-    spark.sql("select * from tweets").show()
-    spark.stop()
-    sc = SparkContext(appName="TwitterStreaming")
-    ssc = StreamingContext(sc, 5)
-    ssc.checkpoint("checkpoint")
-    lines = ssc.socketTextStream("localhost", 9999)
-    words = lines.flatMap(lambda line: line.split(" "))
-    pairs = words.map(lambda word: (word, 1))
-    wordCounts = pairs.reduceByKey(lambda x, y: x + y)
-    wordCounts.pprint()
-    ssc.start()
-    ssc.awaitTermination()
-    ssc.stop()
-    sc.stop()
-    consumer.close()
-    print("Done")
+for msg in consumer:
+    print(json.loads(json.dumps(msg.value)))
 
-    
+# spark = SparkSession.builder.master("local").appName("Tweets").getOrCreate()
+# sc = spark.sparkContext
+# sc.setLogLevel("ERROR")
+# urlRdd = sc.parallelize([consumer])
+# urldf = spark.read.json(urlRdd)
+# urldf.printSchema()
+# urldf.show()
 
+
+# if __name__ == '__main__':
+#     spark = SparkSession.builder.appName("TwitterStreaming").getOrCreate().readStream.format("kafka").option("kafka.bootstrap.servers", "localhost:9092").option("subscribe", topic_name).load()
+#     spark.createOrReplaceTempView("tweets")
+#     spark.sql("select * from tweets").show()
+#     spark.stop()
+#     sc = SparkContext(appName="TwitterStreaming")
+#     ssc = StreamingContext(sc, 5)
+#     ssc.checkpoint("checkpoint")
+#     lines = ssc.socketTextStream("localhost", 9999)
+#     words = lines.flatMap(lambda line: line.split(" "))
+#     pairs = words.map(lambda word: (word, 1))
+#     wordCounts = pairs.reduceByKey(lambda x, y: x + y)
+#     wordCounts.pprint()
+#     ssc.start()
+#     ssc.awaitTermination()
+#     ssc.stop()
+#     sc.stop()
+#     consumer.close()
+#     print("Done")
